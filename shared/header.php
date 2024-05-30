d3b8b897043af676f41c4067e4a22f5f542c9f19
Luis Armenta <luigiangelxd@gmail.com>
2024-05-30 07:03:48 -0600

    style: agregando bootstrap

--- shared/header.php
+++ shared/header.php
@@ -7,7 +7,7 @@
     <title><?php echo isset($title) ? $title: 'Titulo'; ?></title>
     <link rel="stylesheet" href="assets/styles/main.css">
     <script src="https://kit.fontawesome.com/6b47139783.js" crossorigin="anonymous"></script>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <?php 
     if (isset($extra_styles) && count($extra_styles) > 0) {
       for ($i = 0; $i < count($extra_styles); $i++) {
