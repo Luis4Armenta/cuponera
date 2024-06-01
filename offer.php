a9fd21bfaee7c0801765b4ed83b9fb988ad47881
Luis Armenta <luigiangelxd@gmail.com>
2024-06-01 15:00:50 -0600

    feat: conexión a db para offer

--- offer.php
+++ offer.php
@@ -1,32 +1,54 @@
 <?php
-$id = $_GET['id'];
+include_once 'utils.php';
+include_once 'config.php';
+include_once 'Database.php';
+
 if (!isset($_GET['id'])) {
   header('Location: index.html');
   exit;
 }
 
-$date1 = new DateTime();
-$interval = new DateInterval('PT2H15M');
-$date2 = clone $date1;
-$date2->add($interval);
+$data = sanitize_input($_GET, array('id' => 'int'));
+$id = $data['id'];
 
+try {
+  $database = new Database();
+  $db = $database->getConnection();
+  $res = $db->query("SELECT * FROM DealInformation WHERE deal_id = {$id} limit 1;");
 
-$promo = array(
-  'id_post' => '2',
-  'title' => 'Amazon: Escritorio minimalista $799 con Cupón del vendedor.',
-  'actual_price' => 799,
-  'previous_price' => 899,
-  'delivery_price' => 128,
-  'shop' => 'Amazon',
-  'publisher' => 'MICHRO99',
-  'img' => 'https://static.promodescuentos.com/threads/raw/tCdbE/959041_1/re/1024x1024/qt/60/959041_1.jpg',
-  'description' => 'Escritorio minimalista con almacenamiento y nivel para PC.  ',
-  'num_comments' => 13,
-  'url' => 'https://www.amazon.com.mx/dp/B0CR1F46M7',
-  'publication_date' => $date2,
-  'expiration_datetime' => $date1,
+  $offer = array();
+  while ($registro = $res->fetch_row()) {
+    $offer = array(
+      'deal_id' => $registro[0],
+      'title' => $registro[1],  
+      'coupon_code' => $registro[2],  
+      'link' => $registro[3],  
+      'start_date' => $registro[4],  
+      'end_date' => $registro[5],  
+      'start_time' => $registro[6],  
+      'end_time' => $registro[7],  
+      'description' => $registro[8],  
+      'regular_price' => $registro[9],  
+      'offer_price' => $registro[10],  
+      'availability' => $registro[11],  
+      'shipping_cost' => $registro[12],  
+      'shipping_address' => $registro[13],  
+      'store' => $registro[14],
+      'category_name' => $registro[15],
+      'creator_username' => $registro[16],
+      'image_link' => $registro[17],
+    );
+  }
 
-);
+  $res->free_result();
+  $database->closeConnection();
+  if (!isset($offer['deal_id'])) {
+    header('Location: shared/errors/404.php');
+  }
+} catch (mysqli_sql_exception $e) {
+  header('Location: shared/errors/500.php');
+  exit;
+}
 
 
 include 'shared/header.php';
@@ -37,13 +59,12 @@ include 'shared/header.php';
     <div class="card mb-3">
       <div class="row g-0">
         <div class="col-md-4">
-          <img src="<?php echo $promo['img']; ?>" class="img-fluid rounded-start" alt="Imagen del articulo/servicio">
+          <img src="<?php echo $offer['image_link']; ?>" class="img-fluid rounded-start" alt="Imagen del articulo/servicio">
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h5 class="card-title"><?php echo $promo['title']; ?></h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-              content. This content is a little bit longer.</p>
+            <h5 class="card-title"><?php echo $offer['title']; ?></h5>
+            <p class="card-text"><?php echo $offer['description']; ?></p>
             <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
           </div>
         </div>
