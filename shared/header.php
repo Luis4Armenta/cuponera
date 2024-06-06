b0ac98aca4a46f1650097163f24ce8a806b1aec7
Luis Armenta <luigiangelxd@gmail.com>
2024-06-05 22:19:49 -0600

    feat: CDN para DataTable, tooltips, jquery y sweetalert2

--- shared/header.php
+++ shared/header.php
@@ -6,7 +6,8 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
     <title><?php echo isset($title) ? $title : 'Titulo'; ?></title>
     <?php
@@ -16,8 +17,13 @@
       }
     }
     ?>
+    <script>
+      var on_load = () => {
+        return;
+      }
+    </script>
 </head>
-<body>
+<body onload="on_load();">
     <header>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
