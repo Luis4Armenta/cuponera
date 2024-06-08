110b43f4fe020955ee2f7edd305a369ffcaaa0a1
Luis Armenta <luigiangelxd@gmail.com>
2024-06-08 13:16:27 -0600

    style: margen de botones

--- shared/header.php
+++ shared/header.php
@@ -3,7 +3,6 @@
 <head>
 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     
@@ -69,11 +68,11 @@
                   </ul>
                 </li>
               <?php else: ?>
-                <li class="nav-item ms-4">
+                <li class="nav-item ms-4 me-1">
                   <a class="btn btn-primary" href="login.php">Registrarse / Iniciar sesión</a>
                 </li>
               <?php endif; ?>
-              <li class="nav-item me-4 d-flex align-items-center">
+              <li class="nav-item ms-2 me-4 d-flex align-items-center">
                 <a class="btn btn-success btn-lg py-1" href="<?php echo isset($_SESSION['user']) ? 'share.php' : 'login.php'; ?>"><i class="bi bi-plus p-0 m-0 fw-bold"></i>  Compartir</a>
               </li>
             </ul>
