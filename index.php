553ebe0ecb6be427133ed6fefa2befa4ede21303
Luis Armenta <luigiangelxd@gmail.com>
2024-06-08 12:58:29 -0600

    style: paginación para index

--- index.php
+++ index.php
@@ -173,6 +173,19 @@ $promos = [$promo1, $promo2];
         </div>
       </div>
     <?php endforeach; ?>
+    <nav aria-label="Page navigation">
+      <ul class="pagination justify-content-center">
+        <li class="page-item disabled">
+          <a class="page-link">Previous</a>
+        </li>
+        <li class="page-item active"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item">
+          <a class="page-link" href="#">Next</a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </div>
 
