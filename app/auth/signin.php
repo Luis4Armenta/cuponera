77d54cd03f4cd9a1bec49cee8391bd1d717f9a3e
Luis Armenta <luigiangelxd@gmail.com>
2024-06-25 21:15:07 -0600

    style: feedback de inputs

--- app/auth/signin.php
+++ app/auth/signin.php
@@ -20,16 +20,31 @@ include '../shared/header.php';
             <div class="form-group">
               <label for="email">Email</label>
               <input type="email" id="email" name="email" class="form-control">
+              <div class="valid-feedback">
+                ¡Nunca había vito un email tan original!
+              </div>
+              <div class="invalid-feedback" id="email-invalid">
+                Ese email no me parece muy convincente...
+              </div>
             </div>
             <div class="form-group">
               <label for="user">Usuario</label>
               <input type="text" id="user" name="user" class="form-control" required minlength="4" maxlength="20">
+              <div class="valid-feedback">
+                ¡Ese usuario está perfecto!
+              </div>
+              <div class="invalid-feedback" id="user-invalid">
+                Los usuarios solo pueden contener letras, números, guiones y guiones bajos.
+              </div>
             </div>
             <div class="form-group">
               <label for="password">Contraseña</label>
               <input type="password" id="password" name="password" class="form-control" required minlength="8" maxlength="16">
               <div class="invalid-feedback">
                 Ingrese una contraseña válida (debe ser de 8 a 16 caracteres).
+              </div>
+              <div class="valid-feedback">
+                ¡Con eso será suficiente!
               </div>
             </div>
             <p>Si ya tienes una cuenta puede ingresar <a href="/auth/login.php">aquí</a></p>
@@ -45,6 +60,10 @@ include '../shared/header.php';
 
 <script>
   on_load = () => {
+    var success_form = false;
+    var invalid_email = document.getElementById('email-invalid');
+    var invalid_user = document.getElementById('user-invalid');
+
     // Expresión regular para validar el formato de email
     function isValidEmail(email) {
         var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -55,9 +74,29 @@ include '../shared/header.php';
     function validateEmail() {
         var email = $('#email').val();
         if (email !== '' && isValidEmail(email)) {
-            $('#email').removeClass('is-invalid').addClass('is-valid');
+          console.log('siempre')
+          $.ajax({
+                url: '/auth/ajax/check_email.php',
+                method: 'POST',
+                data: { email: email },
+                success: function (response) {
+                    response = JSON.parse(response);
+                    if (response.emailAvailable) {
+                        $('#email').removeClass('is-invalid').addClass('is-valid');
+                    } else {
+                        $('#email').removeClass('is-valid').addClass('is-invalid');
+                        if (invalid_email) {
+                          invalid_email.innerText = 'Ese email parece no estar disponible.';
+                        }
+                    }
+                    validateSubmitButton();
+                }
+            });
             validateSubmitButton();
         } else {
+            if (invalid_email) {
+              invalid_email.innerText = 'Ese email no me parece muy convincente...';
+            }
             $('#email').removeClass('is-valid').addClass('is-invalid');
             validateSubmitButton();
         }
@@ -72,10 +111,29 @@ include '../shared/header.php';
     function validateUser() {
         var user = $('#user').val();
         if (user !== '' && isValidUser(user)) {
-            $('#user').removeClass('is-invalid').addClass('is-valid');
+            $.ajax({
+                url: '/auth/ajax/check_user.php',
+                method: 'POST',
+                data: { user: user },
+                success: function (response) {
+                    response = JSON.parse(response);
+                    if (response.userAvailable) {
+                        $('#user').removeClass('is-invalid').addClass('is-valid');
+                    } else {
+                        $('#user').removeClass('is-valid').addClass('is-invalid');
+                        if (invalid_user) {
+                          invalid_user.innerText = 'Ese usuario parece no estar disponible.';
+                        }
+                    }
+                    validateSubmitButton();
+                }
+            });
             validateSubmitButton();
         } else {
             $('#user').removeClass('is-valid').addClass('is-invalid');
+            if (invalid_user) {
+              invalid_user.innerText = 'Los usuarios solo pueden contener letras, números, guiones y guiones bajos.';
+            }
             validateSubmitButton();
         }
     }
