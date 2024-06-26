7fd2d02b94b79da6784b9847289a2a368ffb2646
Luis Armenta <luigiangelxd@gmail.com>
2024-06-25 18:28:56 -0600

    fix: cambio en el eliminación de ofertas para evitar problemas de sql injection

--- app/offers/actions/delete_offer.php
+++ app/offers/actions/delete_offer.php
@@ -6,41 +6,53 @@ include_once '../../utils.php';
 
 session_start();
 if (!isset($_SESSION['user'])) {
-  Header('Location: ../../auth/login.php');
-  exit;
+    Header('Location: ../../auth/login.php');
+    exit;
 }
 
 $expected_fields = array(
-  'id' => 'int',
+    'id' => 'int',
 );
-
 
 $data = sanitize_input($_POST, $expected_fields);
 
 if (isset($data['id'])) {
-  try {
-    $query = "DELETE FROM DEALS WHERE deal_id = {$data['id']}; ";
+    try {
+        $database = new Database();
+        $db = $database->getConnection();
 
-    $database = new Database();
-    $db = $database->getConnection();
-    $res = $db->query($query);
+        // Preparar la consulta SQL
+        $query = "DELETE FROM deals WHERE deal_id = ?";
 
-    if ($res === TRUE) {
-      echo "Exito";
-    } else {
-      echo "error";
+        $stmt = $db->prepare($query);
+
+        // Verificar si hay errores en la preparación de la consulta
+        if (!$stmt) {
+            echo "Error en la preparación de la consulta: " . $db->error;
+            exit;
+        }
+
+        // Vincular el parámetro a la consulta
+        $stmt->bind_param("i", $data['id']);
+
+        // Ejecutar la consulta
+        if ($stmt->execute()) {
+            echo "Éxito";
+        } else {
+            echo "Error";
+        }
+
+        // Cerrar la declaración y la conexión
+        $stmt->close();
+        $database->closeConnection();
+        exit;
+
+    } catch (mysqli_sql_exception $e) {
+        header('Location: ../../shared/errors/500.php');
+        exit;
     }
-    
-    // $res->free_result();
-    $database->closeConnection();
-    exit;
-
-  } catch (mysqli_sql_exception $e) {
-    header('Location: ../../shared/errors/500.php');
-    exit;
-  }
 } else {
-  Header('Location: ../share.php');
-  exit;
+    Header('Location: ../share.php');
+    exit;
 }
 ?>
